ef898b300d017ffcf7b47a7a2a928ae7e286e6c2
Ross Scarborough <rossscarborough@gmail.com>
2017-09-28 00:10:59 -0400

    Added new media queries for smart devices

--- js/index.js
+++ js/index.js
@@ -1,33 +1,21 @@
 function dropMenu() {
   $('#navbar').toggleClass('drop-menu');
 }
-
 $('a').click(function() {
   if ($('#navbar').hasClass('drop-menu')) {
     $('#navbar').removeClass('drop-menu');
   }
 });
-
 $('.go-btn').click(function() {
   var tgt = $(this).data("scroll-target");
   $('html,body').animate({
     scrollTop: $(tgt).offset().top
   }, 1000);
-})
-/*
-function scrollTo(tgt) {
-  console.log('click');
-  $('html,body').animate({
-    scrollTop: $(tgt).offset().top
-  }, 1000);
-};
-*/
+});
 $(window).scroll(function() {
 	if (($(window).scrollTop() < $('#portfolio').position().top - 10) || ($(window).scrollTop() >= $('#contact').position().top -10)) {
-   // $('#navbar').children().css('color', 'white');
     $('#navbar').children().removeClass('nav-over-portfolio').addClass('nav-over-profile');
   } else {
- //   $('#navbar').children().css('color', 'black');
     $('#navbar').children().removeClass('nav-over-profile').addClass('nav-over-portfolio');
   };
 });
