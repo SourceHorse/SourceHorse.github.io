76b9b592135d2c5e9e8b332b8163f5d184434337
Ross Scarborough <rossscarborough@gmail.com>
2017-10-05 18:00:40 -0400

    Live push for portfolio 2.0 redesign.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,9 @@
-function dropMenu() {
-  $('#navbar').toggleClass('drop-menu');
-}
-$('a').click(function() {
-  if ($('#navbar').hasClass('drop-menu')) {
-    $('#navbar').removeClass('drop-menu');
-  }
-});
 $('.go-btn').click(function() {
   var tgt = $(this).data("scroll-target");
   $('html,body').animate({
     scrollTop: $(tgt).offset().top
   }, 1000);
 });
-$(window).scroll(function() {
-	if (($(window).scrollTop() < $('#portfolio').position().top - 10) || ($(window).scrollTop() >= $('#contact').position().top -10)) {
-    $('#navbar').children().removeClass('nav-over-portfolio').addClass('nav-over-profile');
-  } else {
-    $('#navbar').children().removeClass('nav-over-profile').addClass('nav-over-portfolio');
-  };
+$('.nav a').on('click', function() {
+  $('.collapse').collapse('hide');
 });
--- v2_portfolio/js/index.js
+++
@@ -1,6 +0,0 @@
-$('.go-btn').click(function() {
-  var tgt = $(this).data("scroll-target");
-  $('html,body').animate({
-    scrollTop: $(tgt).offset().top
-  }, 1000);
-});
